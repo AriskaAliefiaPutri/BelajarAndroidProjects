1c9eff500345768c98ea03594f26feadcc5fc26e
AriskaAliefiaPutri <140670433+AriskaAliefiaPutri@users.noreply.github.com>
2024-08-03 17:35:52 +0700

    Video 23

--- RecyclerViewCardView/app/src/main/java/com/example/recyclerviewcardview/MainActivity.java
+++ RecyclerViewCardView/app/src/main/java/com/example/recyclerviewcardview/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.recyclerviewcardview;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -24,7 +25,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.btnTambah), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
@@ -65,4 +66,10 @@ public class MainActivity extends AppCompatActivity {
         adapter = new SiswaAdapter(this,siswaList);
         recyclerView.setAdapter(adapter);
     }
+
+    public void btnTambah(View view) {
+        siswaList.add(new Siswa("Yesaya Bunalven", "Jakarta"));
+        adapter.notifyDataSetChanged();
+    }
+
 }
